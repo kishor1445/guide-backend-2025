a7a42017b27df1077cd9756be50a34b4ab98bf75
Kishor <kishorramanan5@gmail.com>
2023-11-12 23:21:52 +0530

    Update parser.py

--- app/modules/parser.py
+++ app/modules/parser.py
@@ -4,7 +4,18 @@ from flask_restful import reqparse
 def parse_arg(args: tuple):
     arg_p = reqparse.RequestParser()
     for arg in args:
+        if not arg[3]:
+            loc = (
+                "json",
+                "values",
+            )
+        else:
+            loc = arg[3]
         arg_p.add_argument(
-            arg[0], type=arg[1], help=f"{arg[0]} is Required", required=arg[2]
+            arg[0],
+            type=arg[1],
+            help=f"{arg[0]} is Required",
+            required=arg[2],
+            location=loc,
         )
     return arg_p
